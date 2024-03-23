8235b60a80abc084003dbffcf2d56e583a8d52fc
Surendra Shukla <surendra.shukla29@gmail.com>
2024-03-23 10:26:08 +0530

    adds sample code

--- cdk_v2_python_app/cdk_v2_python_app_stack.py
+++ cdk_v2_python_app/cdk_v2_python_app_stack.py
@@ -2,7 +2,9 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_lambda as _lambda,
 )
+
 from constructs import Construct
 
 class CdkV2PythonAppStack(Stack):
@@ -10,10 +12,10 @@ class CdkV2PythonAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkV2PythonAppQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
+        )
